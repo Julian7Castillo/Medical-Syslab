3e85b749155abd668bf821ae29d5d689f3be47da
Julian Castillo <oscarcastillo120@unisangil.edu.co>
2023-09-03 23:29:58 -0500

    Se agregan los scripts de bienvenida predeterminada y agrega login para iniciar sesion

---
+++ Vista/bienvenida.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/styles.css">
+    	<link rel="stylesheet" href="css/bootstrap.min.css">
+		<title>Centro Medico</title>
+	</head>
+	<body>
+		<article class="col-xs-12">
+			<center><h2>Bienvenido a nuestro centro medico virtual</h2><br><br>
+			<p><h3>Gracias por visitar nuestra sitio web esperamos tengas una gran atencion con nuestros servicios y accesibilidad, en la parte superior tendras acceso a las opciones de consultar a los medicos, pacientes, consultorios y citas que se encuentran regitradas o registrar unas nuevas, siempre buscamos lo mejor para nuestros usuarios no olvides dejarnos tus opiniones.</h3></p><br><br>
+			<img src=" img/imagen.jpg"al="logo" style="width:350px;height:250px;"><br><br></center>
+		</article>
+	</body>
+</html>
---
+++ Vista/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!--link de css-->
+		<link rel="stylesheet" href="css/styles.css">
+
+    	<!--llamado a estilos del framwork bootstrap-->
+    	<link rel="stylesheet" href="css/bootstrap.min.css">
+		<title> Medical SysLab</title>
+	</head>
+
+	<body>
+		<!--contenido del formulario del login-->
+		<div class="container">
+			<section class="main row">
+				<article class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+					<form action="../controlador/validacionIniciosesion.php" mothod="post">
+						<p><center><h2>Bienvenido a nuestro centro medico virtual por favor inicie sesion</h2><br><br>
+						<h3>Usuario: <br>
+						<INPUT type="text" name="usuario"><br>
+						Contraseña: <br>
+						<INPUT type="password" name ="psw"><br><br>
+						<button class="btn btn-primary btn-lg" type="submit" >Iniciar sesion</button></h3></center>
+					</p><br><br><br><br></form>
+				</article>
+
+				<aside class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+					<p>
+						<h2>Gracias por visitar nuestra sitio web esperamos tengas una gran atencion con nuestros servicios y accesibilidad, siempre buscamos lo mejor para nuestros usuarios no olvides dejarnos tus opiniones.<h2> 
+					</p>
+				</aside>
+			</section>
+		</div>
+	</body>
+<html>
