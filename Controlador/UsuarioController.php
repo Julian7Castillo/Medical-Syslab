e0e34fcd811d5666111bfb74e93ac209ebbfbfa3
oscarcastillo212002@gmail.com <oscarcastillo212002@gmail.com>
2024-06-07 09:24:29 -0500

    Ajsute de modelo de usuario

--- Controlador/UsuarioController.php
+++ Controlador/UsuarioController.php
@@ -16,57 +16,67 @@
 
 	}
 
-	$objUsuario = New Usuario();
-	$objUsuario->crearUsuario($_POST['rol'], $_POST['identificacion'], $_POST['nombreUsuario'], $_POST['password'], 'Activo');
-	$resultado=$objUsuario->agregarUsuario();
-	
-	if($resultado)
-		header("location:./Vista/inicio.php?pag=ingresoExitoso");
-	else
-		header("location:./Vista/inicio.php?pag=ingresoErroneo");
+	switch($_GET["op"]){
+		case "crearUsuario":
+			$objUsuario = New Usuario();
+			$objUsuario->crearUsuario($_POST['rol'], $_POST['identificacion'], $_POST['nombreUsuario'], $_POST['password'], 'Activo');
+			$resultado=$objUsuario->agregarUsuario();
+			
+			if($resultado)
+				header("location:./Vista/inicio.php?pag=ingresoExitoso");
+			else
+				header("location:./Vista/inicio.php?pag=ingresoErroneo");
+			break;
 
-	//Listar
+		case "ListarUsuario":
+			break;
 
-	//Consultar
+		case "ActualizarUsuario":
+			//Actualizar
+			session_start();
+			extract($_POST);
+			require"../Modelo/Conecta.php";
+			require"../Modelo/ClaseUsuario.php";
+			
+			if($_POST['rol'] == 'Administrador'){
+				$_POST['rol'] = 1;
 
-	//Actualizar
-	session_start();
-	extract($_POST);
-	require"../Modelo/Conecta.php";
-	require"../Modelo/ClaseUsuario.php";
-	
-	if($_POST['rol'] == 'Administrador'){
-		$_POST['rol'] = 1;
+			}if($_POST['rol'] == 'Medico'){
+				$_POST['rol']= 2;
 
-	}if($_POST['rol'] == 'Medico'){
-		$_POST['rol']= 2;
+			}if($_POST['rol'] == 'Paciente'){
+				$_POST['rol'] = 3;
 
-	}if($_POST['rol'] == 'Paciente'){
-		$_POST['rol'] = 3;
+			}
 
+			$objupUsuario=New Usuario();
+			$objupUsuario->crearUsuario($_POST['rol'], $_POST['identificacion'], $_POST['nombreUsuario'], $_POST['password'], $_POST['estado']);
+			$resultado=$objupUsuario->ActualizarUsuario();
+			
+			if($resultado)
+				header("location:../Vista/inicio.php?pag=ingresoExitoso");
+			else
+				header("location:../Vista/inicio.php?pag=ingresoErroneo");
+			break;
+		case "ConsultarUsuario":
+				break;
+
+		case "EliminarUsuario":
+			//Eliminar
+			session_start();
+			extract($_POST);
+			require"../Modelo/Conecta.php";
+			require"../Modelo/ClaseUsuario.php";
+
+			$objUsuario=New Usuario();
+			$objUsuario->crearUsuario($_POST['rol'], $_POST['identificacion'], $_POST['nombreUsuario'], $_POST['password'], $_POST['estado']);
+			$resultado=$objUsuario->BorrarUsuario();
+			
+			if($resultado)
+				header("location:../Vista/inicio.php?pag=ingresoExitoso");
+			else
+				header("location:../Vista/inicio.php?pag=ingresoErroneo");
+			break;
 	}
 
-	$objupUsuario=New Usuario();
-	$objupUsuario->crearUsuario($_POST['rol'], $_POST['identificacion'], $_POST['nombreUsuario'], $_POST['password'], $_POST['estado']);
-	$resultado=$objupUsuario->ActualizarUsuario();
-	
-	if($resultado)
-		header("location:../Vista/inicio.php?pag=ingresoExitoso");
-	else
-		header("location:../Vista/inicio.php?pag=ingresoErroneo");
-
-	//Eliminar
-	session_start();
-	extract($_POST);
-	require"../Modelo/Conecta.php";
-	require"../Modelo/ClaseUsuario.php";
-
-	$objUsuario=New Usuario();
-	$objUsuario->crearUsuario($_POST['rol'], $_POST['identificacion'], $_POST['nombreUsuario'], $_POST['password'], $_POST['estado']);
-	$resultado=$objUsuario->BorrarUsuario();
-	
-	if($resultado)
-		header("location:../Vista/inicio.php?pag=ingresoExitoso");
-	else
-		header("location:../Vista/inicio.php?pag=ingresoErroneo");
 ?>
