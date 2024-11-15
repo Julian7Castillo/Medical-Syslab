ac63106caa9f1793b6e6ea4db11638798614180a
Julian7Castillo <oscarcastillo212002@gmail.com>
2024-11-15 12:04:24 -0500

    ajuste controlador

--- Controlador/LoginController.php
+++ Controlador/LoginController.php
@@ -1,28 +1,69 @@
 <?php
+	function limpiar_cadena($cadena){
+		$cadena = trim($cadena);
+		$cadena = stripcslashes($cadena);
+		$cadena = str_ireplace("<script>","", $cadena);
+		$cadena = str_ireplace("</script>","", $cadena);
+		$cadena = str_ireplace("<script src","", $cadena);
+		$cadena = str_ireplace("<script type=","", $cadena);
+		$cadena = str_ireplace("SELECT * FROM","", $cadena);
+		$cadena = str_ireplace("DELETE FROM","", $cadena);
+		$cadena = str_ireplace("INSERT INTO","", $cadena);
+		$cadena = str_ireplace("DROP TABLE","", $cadena);
+		$cadena = str_ireplace("DROP DATABASE","", $cadena);
+		$cadena = str_ireplace("TRUNCATE TABLE","", $cadena);
+		$cadena = str_ireplace("SHOW TABLES","", $cadena);
+		$cadena = str_ireplace("SHOW DATABASES","", $cadena);
+		$cadena = str_ireplace("<?php","", $cadena);
+		$cadena = str_ireplace("?>","", $cadena);
+		$cadena = str_ireplace("--","", $cadena);
+		$cadena = str_ireplace("<","", $cadena);
+		$cadena = str_ireplace("^","", $cadena);
+		$cadena = str_ireplace(">","", $cadena);
+		$cadena = str_ireplace("[","", $cadena);
+		$cadena = str_ireplace("]","", $cadena);
+		$cadena = str_ireplace("==","", $cadena);
+		$cadena = str_ireplace(";","", $cadena);
+		$cadena = str_ireplace("::","", $cadena);
+		$cadena = trim($cadena);
+		$cadena = stripcslashes($cadena);
+		return $cadena;
+	}
+
 	session_start();
 	extract ($_POST);
-	require "../Modelo/conecta.php";
-	require "../Modelo/ClaseUsuario.php";
+	require ("../Modelo/conecta.php");
+	require ("../Modelo/ClaseUsuario.php");
 
 	$passMedicalSyslab = $_REQUEST['pswMedicalSyslab'];
 	$loginMedicalSyslab = $_REQUEST['usuarioMedicalSyslab'];
 
 	$objConexion=Conectarse();
 
-	$sql="SELECT * FROM usuarios WHERE usucc = '$loginMedicalSyslab' AND usuPassword = '$passMedicalSyslab'";
+	$sql="SELECT * FROM usuarios WHERE usucc = ? AND usuPassword = ?";
+	$stmt = mysqli_prepare($objConexion, $sql);
 
-	$resultado=$objConexion->query($sql);
-	$existe = $resultado->num_rows;
+	if($stmt){
+		$ok = mysqli_stmt_bind_param($stmt, "ss", $loginMedicalSyslab, $passMedicalSyslab);
+		$ok = mysqli_stmt_execute($stmt);	//$sql = limpiar_cadena($sql)
+	
+		$resultado = mysqli_stmt_get_result($stmt);
+		$existe = $resultado->num_rows;
+	
+		if ($existe/*== 1*/) {
+			$usuarioMedicalSyslab=$resultado->fetch_object() or die ("Error");
+			$_SESSION['userMedicalSyslab']= $usuarioMedicalSyslab->usuNombre;
+			$_SESSION['ccMedicalSyslab']= $usuarioMedicalSyslab->usucc;
+			$_SESSION['rolMedicalSyslab']= $usuarioMedicalSyslab->usuRol;
+			header("location:../index.php?pag=inicio");
+		}
+		else{
+			header("location:../index.php?msj=2");  
+			//msj=2, quiere decir que el usuario no esta registrado
+		}
 
-	if ($existe==1) {
-		$usuarioMedicalSyslab=$resultado->fetch_object() or die ("Error");
-		$_SESSION['userMedicalSyslab']= $usuarioMedicalSyslab->usuNombre;
-		$_SESSION['ccMedicalSyslab']= $usuarioMedicalSyslab->usucc;
-		$_SESSION['rolMedicalSyslab']= $usuarioMedicalSyslab->usuRol;
-		header("location:../index.php?pag=inicio");
+	}else{
+		die("Error en la preparación de la consulta");
 	}
-	else{
-		header("location:../index.php?msj=2");  
-		//msj=2, quiere decir que el usuario no esta registrado
-	}
-?>
+
+	mysqli_close($objConexion);
